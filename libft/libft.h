2112b47feb661d50a8c419607cc4c301ff026f67
apple <apple@appledeMacBook-Air.local>
2019-11-20 10:08:46 +0100

    -h

--- libft/libft.h
+++ libft/libft.h
@@ -58,5 +58,8 @@ int ft_isupper(int c);
 int ft_islower(int c);
 t_list *ft_lstnew(void *content);
 void ft_lstadd_front(t_list **alst, t_list *new);
+int ft_lstsize(t_list *lst);
+t_list *ft_lstlast(t_list *lst);
+void ft_lstadd_back(t_list **alst, t_list *new);
 
 #endif
