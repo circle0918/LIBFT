0cc5c9b723c61cef8f39a15ce62714ee79359b6e
apple <apple@appledeMacBook-Air.local>
2019-12-07 13:16:48 +0100

    -ajouter test

--- libft/ft_lstiter.c
+++ libft/ft_lstiter.c
@@ -1,39 +1,44 @@
 #include "libft.h"
+#include <stdio.h>
 
 /* #1 L’adresse du pointeur vers un élément.
  * #2. L’adresse de la fonction à appliquer.
  * Itère sur la list lst et applique la fonction f au
  * contenu chaque élément.*/
+void ff(t_list *content)
+{
+    char *s = content->content;
+    printf("inside fp: %p\n",s);
+    printf("inside %s\n",(s));
 
-// void *(f)(void *content)
-// {
-//     char *s = content;
+    s[0] = 'O';
+    s[1] = 'K';
+}
 
-//     s[0] = 'O';
-//     s[1] = 'K';
-// }
+void ft_lstiter(t_list *lst, void (*f)(void *))
+{
+    t_list *tmp;
+    t_list *new;
 
-// void ft_lstiter(t_list *lst, void (*f)(void *))
-// {
-//     t_list *tmp;
-//   //  t_list *new;
+    if (!lst || !f)
+        return;
+    tmp = lst;
+    while (tmp)
+    {
+        new = tmp->next;
+        //printf("f : %s\n",tmp->content);
+        //printf("fp: %p\n",tmp);
+        f(tmp);
+        //printf("b : %s\n",tmp->content);
+        tmp = new;
+    }
+}
 
-//     if (!lst || !f)
-//         return;
-//     tmp = lst;
-//     while (tmp)
-//     {
-//         new = tmp->next;
-//         f(tmp);
-//         tmp = new;
-//     }
-// }
+int main()
+{
+    t_list *l1 = ft_lstnew(strdup("he"));
+    l1->next = ft_lstnew(strdup("llo"));
+    ft_lstiter(l1, (void *)ff);
 
-// int main()
-// {
-//     t_list *l1 = ft_lstnew(strdup("he"));
-//     l1->next = ft_lstnew(strdup("llo"));
-//     ft_lstiter(l1, f);
-
-//     printf("%s", l1->next->content);
-// }
+    //printf("%s", l1->content);
+}
