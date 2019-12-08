c755d596c991cceb8600d045305c654c6c83f435
circle0918 <53450239+circle0918@users.noreply.github.com>
2019-12-08 17:40:45 +0100

    Update ft_atoi.c

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -11,46 +11,64 @@
 /* ************************************************************************** */
 #include "libft.h"
 
-static int ft_is_space(char c)
+static int	ft_is_space(char c)
 {
-    return (c == ' ' || c == '\n' || c == '\t' ||
-            c == '\v' || c == '\f' || c == '\r');
+	return (c == ' ' || c == '\n' || c == '\t' ||
+			c == '\v' || c == '\f' || c == '\r');
 }
 
-int ft_atoi(const char *str)
+static	int	return_res(const char *s, int neg, long int res)
 {
-    int neg;
-    long int res;
+	int count;
 
-    neg = 1;
-    res = 0;
-    while (ft_is_space(*str) == 1)
-        str++;
-    if (*str == '-')
-    {
-        neg = -1;
-        str++;
-        if (*str < '0' || *str > '9')
-            return (0);
-    }
-    if (*str == '+')
-        str++;
-    while (*str && *str >= '0' && *str <= '9')
-    {
-        res = res * 10 + (*str - 48);
-        str++;
-        if (res * neg < -2147483648)
-            return (0);
-        else if (res * neg > 2147483647)
-            return (-1);
-    }
+	count = 0;
+	while (s[count] && s[count]>= '0' && s[count] <= '9')
+	{
+		count++;
+	}
+	if(count >= 20 && neg == 1)
 
-    return (int)(res * neg);
+		return(-1);
+	else if(count >= 20 && neg == -1)
+		return (0);
+	// else if (res * neg < -2147483648)
+	// 		return (0);
+	// else if (res * neg > 2147483647)
+	// 		return (-1);
+	else
+			return(neg*res);
 }
 
-int main(int ac, char **av)
+int			ft_atoi(const char *str)
 {
-    printf("%d\n", atoi("1233333333333333333"));
-    printf("%d\n", ft_atoi("1233333333333333333"));
-    return 0;
+	long	int		res;
+	int				neg;
+	const	char	*s;
+
+	neg = 1;
+	res = 0;
+	while (ft_is_space(*str) == 1)
+		str++;
+	if (*str == '-')
+	{
+		neg = -1;
+		str++;
+		if (*str < '0' || *str > '9')
+			return (0);
+	}
+	if (*str == '+')
+		str++;
+	s = str;
+	while (*str && *str >= '0' && *str <= '9')
+	{
+		res = res * 10 + (*str - 48);
+		str++;
+	}
+	return (int)(return_res(s, neg, res));
 }
+// int main(int ac, char **av)
+// {
+//    // printf("%d\n", atoi("21474836478"));
+//     printf("%d\n", ft_atoi("21474836478"));
+//     return 0;
+// }
